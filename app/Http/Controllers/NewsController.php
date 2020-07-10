1a76250f12b196d15c651de8b5afbf33af069788
dnsdnz <berfz23@gmail.com>
2020-07-10 16:25:15 +0300

    first commit

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\News;
+
+class NewsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $news = News::all();
+        return response()->json($news);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+         $news = new News();
+        $news->title = $request->title;
+        $news->content = $request->content;
+        $news->category = $request->category;
+        $news->pic_name = $request->pic_name;
+        $product->save();
+        return response()->json('New added succesfuly.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+         $news = News::find($id);
+        return response()->json($news);    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $news = News::find($id);
+        $news->title = $request->title;
+        $news->content = $request->content;
+        $news->category = $request->category;
+        $news->pic_name = $request->pic_name;
+        $product->update();
+        return response()->json('New updated succesfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        News::destroy($id);
+        return response()->json('New deleted succesfully.');    }
+}
